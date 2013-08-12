909d9418d01477a19319c923340771551a941006
Christian Krause <kizkizzbangbang@googlemail.com>
2013-08-12 15:57:43 +0200

    add help to submit-wrapper

--- submit-wrapper.sh
+++ submit-wrapper.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-qsub $SUBMIT_OPTS @datadir@/submit-scripts/@submitscript@ "$@"
+
+SUBMIT_SCRIPT="@datadir@/submit-scripts/@submitscript@"
+
+[[ $1 = "-h" || $1 = "-help" || $1 = "--help" ]] && {
+cat << EOF
+Submitter Usage
+===============
+
+Submit the job without submit arguments:
+
+    $(basename $0) [app-args]
+
+or explicitly with submit arguments:
+
+    [SUBMIT_OPTS="submit-args"] $(basename $0) [app-args]
+
+or implicitly with submit arguments from the shell variable:
+
+    export SUBMIT_OPTS="submit-args"
+    $(basename $0) [app-args]
+
+Submit Script Usage
+===================
+
+$(bash "$SUBMIT_SCRIPT" $1 | sed 's/^/    /')
+
+EOF
+exit 0
+}
+
+set -x
+
+qsub $SUBMIT_OPTS "$SUBMIT_SCRIPT" "$@"
