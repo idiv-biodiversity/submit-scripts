05a725686fc13e3fe99b6f2780a5f3c3d7dc7fa3
Christian Krause <christian.krause@idiv.de>
2013-08-13 17:19:56 +0200

    vcftools submit scripts

---
+++ src/main/script/vcftools-recode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
+#$ -j y
+
+[[ $1 = "-h" || $1 = "-help" || $1 = "--help" ]] && {
+  echo "usage: <submit-command> [submit-args] $(basename $0) input.vcf.gz output.vcf.gz [vcftools-arguments]"
+  exit 0
+}
+
+source @libdir@/submit-scripts/util.sh
+source /etc/profile.d/000-modules.sh
+module load vcftools
+module load pigz
+
+INPUT_VCF=$1  ; shift
+OUTPUT_VCF=$1 ; shift
+
+${TRACE:+tracer} vcftools --gzvcf $INPUT_VCF "$@" --recode-to-stream | write-to $OUTPUT_VCF
---
+++ src/main/script/vcftools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
+#$ -j y
+
+[[ $1 = "-h" || $1 = "-help" || $1 = "--help" ]] && {
+  echo "usage: <submit-command> [submit-args] $(basename $0) input.vcf.gz output [$(basename $0)-arguments]"
+  exit 0
+}
+
+source @libdir@/submit-scripts/util.sh
+source /etc/profile.d/000-modules.sh
+module load vcftools
+
+INPUT_VCF=$1 ; shift
+OUTPUT=$1    ; shift
+
+${TRACE:+tracer} vcftools --gzvcf $INPUT_VCF "$@" --out $OUTPUT
