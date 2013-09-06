aedd5b11f057f3cc0fddd2f4acb32aa13fb5674c
Christian Krause <christian.krause@idiv.de>
2013-09-06 14:46:38 +0200

    cutadapt submit scripts

---
+++ src/main/script/cutadapt-bare.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
+#$ -j y
+
+[[ $1 = "-h" || $1 = "-help" || $1 = "--help" ]] && {
+cat << EOF
+<submit-command> [submit-args] $0 input output [cutadapt-arguments]"
+
+required arguments
+
+    input                          used as cutadapt input file
+    output                         used as cutadapt output file
+
+EOF
+exit 0
+}
+
+source @libdir@/submit-scripts/util.sh
+source /etc/profile.d/000-modules.sh
+module load cutadapt
+
+INPUT="$1"  ; shift
+OUTPUT="$1" ; shift
+
+${TRACE:+tracer} cutadapt "$INPUT" -o "$OUTPUT" "$@"
